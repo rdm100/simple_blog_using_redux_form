52a42b3fcc106edf5af92814181be328e50abda1
rdm100 <robertode_marco@hotmail.com>
2018-09-26 14:45:26 +0100

    Imported field component into PostsNew component. Use the field component to make the title form field and started the renderTitleField function.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,17 +1,44 @@
 import React, {Component} from 'react';
-import {Form, reduxForm} from 'redux-form';
+import {Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
+	renderTitleField(field){
+	return(
+		<div>
+			<input
+				type="text"
+				{...field.input}
+			/>
+		</div>
+		);
+	}
+
 	render() {
 		return(
 			<div>
-				PostsNew
+				<form>
+					<Field
+						name="title"
+						component={this.renderTitleField}
+					/>
+				</form>
 			</div>
 		);
 	}
 }
 // This is like the connect function from react-redux
-// this allows our helper to help reduxForm communicate directly from the component to the reducer
+// this allows our reduxForm helper to wrap the PostsNew Component so it can communicate directly from the component to the reducer
 export default reduxForm({
 	form: 'PostsNewForm'
 })(PostsNew);
+
+//the field component is used for event handling and updating different pieces of state and it's up to us to define the component
+//prop which will return some amount of jsx that represents the actual element that gets rendered to the screen 
+
+//field.input is an object that contains a bunch of event handlers onChange, onFocus and props like  and field which is the 
+//value of the input
+
+//the field argument contains an event handler or 2 that is going to be responsible for making sure that this Field component 
+//knows that it is responsible for dealing with this particular text input here 
+
+//the ... means field.input is an object here and I want all of the different properties in it to be communicated as props to the input tag
