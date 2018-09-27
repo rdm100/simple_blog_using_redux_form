f5e2f46e674cceb8764cafa563079085d847e4f9
rdm100 <robertode_marco@hotmail.com>
2018-09-27 14:42:39 +0100

    Added conditional styling to form that shows when the form is filled in incorrectly.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,17 +1,22 @@
 import React, {Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
+import {Link} from 'react-router-dom';
 
 class PostsNew extends Component {
 	renderField(field){
+	const className = `form-group ${field.meta.touched && field.meta.error ? 'has-danger' : ''}`;
+
 	return(
-		<div className="form-group">
+		<div className={className}>
 			<label>{field.label}</label>
 			<input
 				className="form-control"
 				type="text"
 				{...field.input}
 			/>
+			<div className="text-help">
 			{field.meta.touched ? field.meta.error : ''}
+			</div>
 		</div>
 		);
 	}
